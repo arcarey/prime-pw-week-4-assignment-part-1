d824998fc4581402e46bc70ccf1f7d62402fac8d
Andrew Carey <andrew.r.carey@gmail.com>
2022-10-10 12:30:39 -0500

    finished q 1-4

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,22 +15,23 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return `Hello, ${name}!`;
 }
 // Remember to call the function to test
-
+console.log(helloName('Mavis'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber ) {
   // return firstNumber + secondNumber;
+  return firstNumber + secondNumber;
 }
-
+console.log(addNumbers(3,-6));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree(firstNumber, secondNumber, thirdNumber ){
+  return firstNumber * secondNumber * thirdNumber;
 }
-
+console.log(multiplyThree(5, 6, -10));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
