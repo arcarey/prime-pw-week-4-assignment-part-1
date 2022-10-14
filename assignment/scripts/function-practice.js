71d2f6dc20b6475fcb9c96285c1f693bf8fef546
Andrew Carey <andrew.r.carey@gmail.com>
2022-10-13 20:19:48 -0500

    replaced a couple if statements with ? :

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -80,12 +80,19 @@ console.log("testArray contains the value 0:", find(0, testArray));
 // ----------------------
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
+// function isFirstLetter(letter, string) {
+//   if (string[0] == letter) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// rewrite function as a ternary for practice 
 function isFirstLetter(letter, string) {
-  if (string[0] == letter) {
-    return true;
-  }
-  return false;
+  return string[0] == letter ? true : false ;
 }
+
+
 console.log("isFirstLetter - should say true", isFirstLetter("a", "apple"));
 console.log("isFirstLetter - should say false", isFirstLetter("z", "apple"));
 
